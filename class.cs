ba758ea5dc2d27be28bfe2844a9e98d7a5dc9ac7
ahmad hadizadeh <giv.m@yahoo.com>
2022-02-01 19:44:14 +0330

    change class

---
+++ class.cs
@@ -0,0 +1,13 @@
+public class ReCaptchaResponse {
+    [JsonProperty ("success")]
+    public bool Success { get; set; }
+
+    [JsonProperty ("challenge_ts")]
+    public string ValidatedDateTime { get; set; }
+
+    [JsonProperty ("hostname")]
+    public string HostName { get; set; }
+
+    [JsonProperty ("error-codes")]
+    public List<string> ErrorCodes { get; set; }
+}
